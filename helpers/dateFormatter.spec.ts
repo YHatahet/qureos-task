bd1f644d223f3cd78312ada7382e344daea616a6
yhatatet <yamen.hatahet@gmail.com>
2022-05-16 14:15:44 +0400

    Cleaned up dateFormatter test file

--- helpers/dateFormatter.spec.ts
+++ helpers/dateFormatter.spec.ts
@@ -1,19 +1,25 @@
 import dateFormatter from "./dateFormatter";
 
 it("trying 5th of May, 2022", () => {
-  expect(dateFormatter(new Date("2022-05-05"))).toBe("5-May-2022");
+  const dateObject = new Date("2022-05-05");
+  const expectedFormattedDate = "5-May-2022";
+  expect(dateFormatter(dateObject)).toBe(expectedFormattedDate);
 });
 
 it("trying 15th of March, 2000", () => {
-  expect(dateFormatter(new Date("2000-03-15"))).toBe("15-March-2000");
+  const dateObject = new Date("2000-03-15");
+  const expectedFormattedDate = "15-March-2000";
+  expect(dateFormatter(dateObject)).toBe(expectedFormattedDate);
 });
 
 it("trying 31st of Dec, 2012", () => {
-  expect(dateFormatter(new Date("2012-12-31:23:59:59"))).toBe(
-    "31-December-2012"
-  );
+  const dateObject = new Date("2012-12-31:23:59:59");
+  const expectedFormattedDate = "31-December-2012";
+  expect(dateFormatter(dateObject)).toBe(expectedFormattedDate);
 });
 
 it("trying 1st of Jan, 1933", () => {
-  expect(dateFormatter(new Date("1933-01-01:00:00:00"))).toBe("1-January-1933");
+  const dateObject = new Date("1933-01-01:00:00:00");
+  const expectedFormattedDate = "1-January-1933";
+  expect(dateFormatter(dateObject)).toBe(expectedFormattedDate);
 });
